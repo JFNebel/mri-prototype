ea1aa8d7a177b5729d37b3f7db01e7d32884aa3d
enmanuelmag <enmanuelmag@cardor.dev>
2022-08-04 21:23:50 +0200

    Update URL

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,4 +1,4 @@
-const URL_SERVER = 'https://a9f5-2001-1458-204-1-00-102-7780.eu.ngrok.io';
+const URL_SERVER = 'https://2eb4-2001-1458-204-1-00-101-7780.eu.ngrok.io';
 
 
 export async function sendFile({ file }) {
